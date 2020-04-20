80c67ffb53adf2367f46c47c37dfc36b8af22217
Frédéric Rouffet <frouffet@gmail.com>
2020-04-20 10:17:27 +0200

    finalisation dossier api-php

--- api-php/quizz.php
+++ api-php/quizz.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 
 $dbh = new PDO('mysql:dbname=quizzangular;host=127.0.0.1', 'root', 'root');
-$sth = $dbh->prepare("SELECT * FROM tw");
+$sth = $dbh->prepare("SELECT * FROM general");
 $sth->execute();
 
 header('Content-type: application/json');
--- api-php/quizz_insert.php
+++ api-php/quizz_insert.php
@@ -4,17 +4,24 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
-if (isset($_POST['message'])) {
-    $dbh = new PDO('mysql:dbname=tw;host=127.0.0.1', 'root', 'root');
-    $sth = $dbh->prepare("INSERT INTO tw (id, message) VALUES (null, :message)");
+if (isset($_POST['theme']) && isset($_POST['question']) && isset($_POST['prop1']) && isset($_POST['prop2']) && isset($_POST['prop3']) && isset($_POST['prop4']) && isset($_POST['difficulte']) && isset($_POST['anecdote'])) {
+    $dbh = new PDO('mysql:dbname=quizzangular;host=127.0.0.1', 'root', 'root');
+    $sth = $dbh->prepare("INSERT INTO generals (id, theme, question, prop1, prop2, prop3, prop4, difficulte, anecdote) VALUES (null, :theme, :question, :prop1, :prop2, :prop3, :prop4, :difficulte, :anecdote)");
 
     $sth->execute(array(
-        'message' => $_POST['message']
+        'theme' => $_POST['theme'],
+        'question' => $_POST['question'],
+        'prop1' => $_POST['prop1'],
+        'prop2' => $_POST['prop2'],
+        'prop3' => $_POST['prop3'],
+        'prop4' => $_POST['prop4'],
+        'difficulte' => $_POST['difficulte'],
+        'anecdote' => $_POST['anecdote']
     ));
 
     header('Content-type: application/json');
 
-    $sth = $dbh->prepare("SELECT * FROM tw");
+    $sth = $dbh->prepare("SELECT * FROM generals");
     $sth->execute();
     echo json_encode($sth->fetchAll());
 } else {
